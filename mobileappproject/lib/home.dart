dc1e69c61d7519fbf7dfe4ee709deea509f26106
mevyst2003 <6531501032@lamduan.mfu.ac.th>
2024-10-19 21:12:50 +0700

    HomePage

---
+++ mobileappproject/lib/home.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Transform.scale(
+          scale: 1.3,
+          child: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/home.jpg"),
+                  fit: BoxFit.cover,
+                  alignment: Alignment(0, -60)),
+            ),
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 50),
+            const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Text(
+                      "Hertz Rental",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 30,
+                      ),
+                    ),
+                    Text(
+                      ".",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red,
+                        fontSize: 50,
+                      ),
+                    ),
+                  ],
+                )),
+            const Spacer(),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.black.withOpacity(0.01),
+                    Colors.black.withOpacity(0.7),
+                    Colors.black.withOpacity(0.9),
+                    Colors.black.withOpacity(1),
+                    Colors.black.withOpacity(1),
+                    Colors.black.withOpacity(1),
+                    Colors.black.withOpacity(1),
+                    Colors.black.withOpacity(1),
+                    Colors.black.withOpacity(1),
+                    Colors.black.withOpacity(1),
+                    Colors.black.withOpacity(1),
+                    Colors.black.withOpacity(1),
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Rent your car.",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 30,
+                        height: 1.0,
+                      ),
+                    ),
+                    const Text(
+                      "Raise your luxury life",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 30,
+                        height: 1.0,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      "Premium and prestige car daily rental.",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 156, 156, 156),
+                        fontSize: 13,
+                        height: 1.0,
+                      ),
+                    ),
+                    const Text(
+                      "Experience the thrill at a lower price.",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 156, 156, 156),
+                        fontSize: 13,
+                        height: 1.0,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: SizedBox(
+                        width: 400,
+                        child: FilledButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            backgroundColor:
+                                WidgetStateProperty.all<Color>(Colors.white),
+                          ),
+                          child: const Text(
+                            "Let's Go!",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ));
+  }
+}
