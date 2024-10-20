e421db4fe40cf9656533044bfaf5831a0f3d895f
mevyst2003 <6531501032@lamduan.mfu.ac.th>
2024-10-20 09:08:32 +0700

    Update loign.dart

--- mobileappproject/lib/loign.dart
+++ mobileappproject/lib/loign.dart
@@ -1,10 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Loign extends StatelessWidget {
   const Loign({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(color: Colors.white),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 30),
+              const Text(
+                "Welcome back!",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                  fontSize: 30,
+                  height: 1.0,
+                ),
+              ),
+              const Text(
+                "Login to your account",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 156, 156, 156),
+                  fontSize: 15,
+                  height: 1.5,
+                ),
+              ),
+              const SizedBox(height: 50),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.grey),
+                ),
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.person_outline_rounded,
+                      color: Color.fromARGB(255, 156, 156, 156),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Login",
+                          hintStyle: TextStyle(
+                              color: Color.fromARGB(255, 156, 156, 156),
+                              fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 13),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.grey),
+                ),
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.lock_outline_rounded,
+                      color: Color.fromARGB(255, 156, 156, 156),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Password",
+                          hintStyle: TextStyle(
+                              color: Color.fromARGB(255, 156, 156, 156),
+                              fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              FilledButton(onPressed: (){}, child: Text("Login"))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
