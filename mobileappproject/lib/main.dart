531275135212a4742e2cf30fb5c7d249e5f4bd9d
Ikkyu027 <6531501027@lamduan.mfu.ac.th>
2024-10-21 22:04:25 +0700

    create ChooseCar1
    create ChooseeCar1

--- mobileappproject/lib/main.dart
+++ mobileappproject/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mobileappproject/ChooseCar1.dart';
 import 'package:mobileappproject/home.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Home(),
+    home: Choosecar1(),
     debugShowCheckedModeBanner: false, //ปิดแบนเนอร์ debug mode ด้านขวาบน,
   ));
 }
