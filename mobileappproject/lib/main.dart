0e8a5103469917b3fffff4e9edf20fad97651a77
mevyst2003 <6531501032@lamduan.mfu.ac.th>
2024-10-26 10:26:36 +0700

    Update main.dart

--- mobileappproject/lib/main.dart
+++ mobileappproject/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:mobileappproject/home.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Cartypelist1(),
+    home: Home(),
     debugShowCheckedModeBanner: false, //ปิดแบนเนอร์ debug mode ด้านขวาบน,
   ));
 }
