ebd92cd0823c4ab3a0ea6ed04512213c029ba17f
mevyst2003 <6531501032@lamduan.mfu.ac.th>
2024-10-28 20:22:47 +0700

    add db and server luncher

---
+++ mobileappproject/server/db.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2');
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'hertzrental'
+});
+
+module.exports = con;
